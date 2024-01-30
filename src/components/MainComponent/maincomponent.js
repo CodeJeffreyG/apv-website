2ff688237b62edaacaecbffd2a8552edca2e10fb
Jeffrey <jeffreygraham600@gmail.com>
2024-01-30 14:51:57 -0500

    update

--- src/components/MainComponent/maincomponent.js
+++ src/components/MainComponent/maincomponent.js
@@ -6,25 +6,8 @@ import cat from "../media/carousel-images/cat.jpg";
 
 const MainComponent = () => {
   return (
-    <div className="main-hero">
-      <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100" src={turtle} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={cat} alt="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        {/* Add more Carousel.Item components as needed */}
-      </Carousel>
-    </div>
+    <div className="main-hero">{/* fuck react bootstrap try swiper  */}</div>
   );
 };
+
 export default MainComponent;
