2849fe1fb9d54a86e5ad1922012d0e8b02e133c9
Jeffrey <jeffreygraham600@gmail.com>
2024-02-01 19:24:56 -0500

    hours component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/navbar.js";
 import MainComponent from "./components/MainComponent/maincomponent.js";
+import Hospitalhours from "./components/HospitalHours/hospitalhours.js";
 function App() {
   return (
     <div className="page-container">
       <Navbar />
       <MainComponent />
+      <Hospitalhours />
     </div>
   );
 }
---
+++ src/components/HospitalHours/hospitalhours.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./hospitalhours.css";
+
+const hospitalhours = () => {
+  return (
+    <div className="hours-container">
+      <div className="hours-leftPanel">
+        <h2>Hospital Hours:</h2>
+        <p>Monday: 9:00AM - 4:00PM</p>
+        <p>Tuesday: 9:00AM - 4:00PM</p>
+        <p>Wednesday: 9:00AM - 6:00PM</p>
+        <p>Thursday: 2:00PM - 8:00PM</p>
+        <p>Friday: 9:00AM - 6:00PM</p>
+        <p>Saturday: 9:00AM - 3:00PM (Every other Saturday)</p>
+      </div>
+      <div className="hours-rightPanel">
+        <h2>Our priority is to minimize stress.</h2>
+        <p className="please-call-important">
+          Please call when you arrive so we can prepare for your visit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default hospitalhours;
