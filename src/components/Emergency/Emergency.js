9bbbb8dac7745534c118b0928a8ca1bf60352116
Jeffrey <jeffreygraham600@gmail.com>
2024-03-05 14:01:41 -0500

    media queries

--- src/components/Emergency/Emergency.js
+++ src/components/Emergency/Emergency.js
@@ -16,52 +16,56 @@ const Emergency = () => {
       <div className="emergency-lists">
         <div className="emergency-column">
           <div className="emergency-type">Emergency Hospitals:</div>
-          <a href="#" className="emergency-link">
+          <a
+            href="https://www.thrivepetcare.com/locations/pennsylvania/havertown/keystone-veterinary-emergency-referral?utm_source=google&utm_medium=gmbs"
+            target="_blank"
+            className="emergency-link"
+          >
             Keystone Veterinary Emergency
             <span className="emergency-link-info">
-              (Havertown, PA 19083 - 24hr)
+              (Havertown, PA, 19083 - 24hr)
             </span>
           </a>
-          <a href="#" className="emergency-link">
+          <a href="#" target="_blank" className="emergency-link">
             Blue Pearl Pet Hospital
             <span className="emergency-link-info">
-              (Philadelphia, PA 19147 - 24hr)
+              (Philadelphia, PA, 19147 - 24hr)
             </span>
           </a>
-          <a href="#" className="emergency-link">
+          <a href="#" target="_blank" className="emergency-link">
             Metropolitan Veterinary Associates
             <span className="emergency-link-info">
-              (Norristown, PA 19403 - 24hr)
+              (Norristown, PA, 19403 - 24hr)
             </span>
           </a>
-          <a href="#" className="emergency-link">
+          <a href="#" target="_blank" className="emergency-link">
             VRC - Veterinary Referral Center
             <span className="emergency-link-info">
-              (Malvern, PA 19355 - 24hr)
+              (Malvern, PA, 19355 - 24hr)
             </span>
           </a>
         </div>
         <div className="emergency-column">
           <div className="emergency-type">For Our Exotic Patients:</div>
-          <a href="#" className="emergency-link">
+          <a href="#" target="_blank" className="emergency-link">
             VEG - Veterinary Emergency Group {/*philadelphia PA  24hr*/}
             <span className="emergency-link-info">
-              (Philadelphia, PA - 24hr)
+              (Philadelphia, PA, 19107 - 24hr)
             </span>
           </a>
           <a href="#" className="emergency-link">
             Mount Laurel Animal Hospital {/* Mount Larurel Township NJ 24 hr*/}
             <span className="emergency-link-info">
-              (Mount Larurel Township, NJ - 24hr)
+              (Mount Larurel Township, NJ, 08054 - 24hr)
             </span>
           </a>
           <a href="#" className="emergency-link">
             Radnor Veterinary Hospital {/* Wayne PA */}
-            <span className="emergency-link-info">(Wayne, PA)</span>
+            <span className="emergency-link-info">(Wayne, PA, 19087)</span>
           </a>
           <a href="#" className="emergency-link">
             Avian & Exotic Medical Center {/* Aston, PA */}
-            <span className="emergency-link-info">(Aston, PA)</span>
+            <span className="emergency-link-info">(Aston, PA, 19130)</span>
           </a>
         </div>
       </div>
