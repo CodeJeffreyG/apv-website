11424f920e94b86425b594f381950e04e1b33d2b
Jeffrey <jeffreygraham600@gmail.com>
2024-03-05 13:03:31 -0500

    emergency adjustments

--- src/components/Emergency/Emergency.js
+++ src/components/Emergency/Emergency.js
@@ -4,27 +4,45 @@ import React from "react";
 const Emergency = () => {
   return (
     <div className="emergency-container">
-      <div className="emergency-header">Emergency Hospitals in our area:</div>
-      <div className="emergency-instructions">
-        If the emergency is during our office hours, call and we will help your
-        pet. If this is an off-hours or overnight emergency, we recommend the
-        following facilities:
+      <div className="emergency-header-container">
+        <div className="emergency-header">WE WILL HELP YOUR PET</div>
+        <div className="emergency-instructions">
+          If the emergency is during our office hours, call and we will help
+          your pet. If this is off our hours or an overnight emergency, we
+          recommend the following facilities:
+        </div>
       </div>
+
       <div className="emergency-lists">
         <div className="emergency-column">
-          <div className="emergency-type">Only Cats & Dogs:</div>
+          <div className="emergency-type">Emergency Hospitals:</div>
           <a href="#" className="emergency-link">
             Keystone Veterinary Emergency
+            <span className="emergency-link-info">
+              (Havertown, PA 19083 - 24hr)
+            </span>
           </a>
           <a href="#" className="emergency-link">
             Blue Pearl Pet Hospital
+            <span className="emergency-link-info">
+              (Philadelphia, PA 19147 - 24hr)
+            </span>
           </a>
           <a href="#" className="emergency-link">
             Metropolitan Veterinary Associates
+            <span className="emergency-link-info">
+              (Norristown, PA 19403 - 24hr)
+            </span>
+          </a>
+          <a href="#" className="emergency-link">
+            VRC - Veterinary Referral Center
+            <span className="emergency-link-info">
+              (Malvern, PA 19355 - 24hr)
+            </span>
           </a>
         </div>
         <div className="emergency-column">
-          <div className="emergency-type">Only Exotic:</div>
+          <div className="emergency-type">For Our Exotic Patients:</div>
           <a href="#" className="emergency-link">
             VEG - Veterinary Emergency Group {/*philadelphia PA  24hr*/}
             <span className="emergency-link-info">
@@ -43,6 +61,7 @@ const Emergency = () => {
           </a>
           <a href="#" className="emergency-link">
             Avian & Exotic Medical Center {/* Aston, PA */}
+            <span className="emergency-link-info">(Aston, PA)</span>
           </a>
         </div>
       </div>
