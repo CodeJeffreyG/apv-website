3bc47ce83ee7107f20616d1d22cef87dac069fff
Jeffrey <jeffreygraham600@gmail.com>
2024-03-12 14:00:11 -0400

    maybe hero container?

--- src/components/MainComponent/HospitalHours/hospitalhours.js
+++ src/components/MainComponent/HospitalHours/hospitalhours.js
@@ -13,14 +13,7 @@ const hospitalhours = () => {
         <p>Friday: 9:00AM - 6:00PM</p>
         <p>Saturday: 9:00AM - 3:00PM (Every other Saturday)</p>
       </div>
-      <div className="hours-rightPanel">
-        <h2>Our priority is to minimize stress.</h2>
-
-        <p className="please-call-important">
-          Please Call: <a href="tel:+2153473997">(484)-461-7520</a>
-          <br /> when you arrive so we can prepare for your visit.
-        </p>
-      </div>
+      
     </div>
   );
 };
--- src/components/MainComponent/maincomponent.js
+++ src/components/MainComponent/maincomponent.js
@@ -21,34 +21,47 @@ import "swiper/css/effect-fade";
 const MainComponent = () => {
   return (
     <div className="main-container">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={2}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-        modules={[Autoplay]}
-        autoplay={true}
-        duration={50}
-        effect="fade"
-        className="main-swiper-container"
-      >
-        {combinedPhotos.map((currentImage, index) => {
-          return (
+      <div className="left-side">
+        <HospitalHours />
+      </div>
+      <div className="right-side">
+        <div className="hours-rightPanel">
+          {/* Content of hours-rightPanel from HospitalHours component */}
+          {/* This will be displayed above the Swiper component */}
+          <h2>Our priority is to minimize stress.</h2>
+          <p className="please-call-important">
+            Please Call: <a href="tel:+2153473997">(484)-461-7520</a>
+            <br /> when you arrive so we can prepare for your visit.
+          </p>
+        </div>
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={2}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          modules={[Autoplay]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          effect="fade"
+          className="main-swiper-container"
+        >
+          {combinedPhotos.map((currentImage, index) => (
             <SwiperSlide
-              index={index}
+              key={index}
               className="main-individual-slide-container"
             >
               <img
                 src={currentImage}
-                alt=""
+                alt={`Slide ${index}`}
                 className="main-individual-slide-image"
               />
             </SwiperSlide>
-          );
-        })}
-      </Swiper>
-      <CenteredOverlay />
-      <HospitalHours />
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
