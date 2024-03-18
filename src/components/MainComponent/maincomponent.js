faf147c2c22b8c2e3f56e0d95109da9d588d2d53
Jeffrey <jeffreygraham600@gmail.com>
2024-03-18 15:49:17 -0400

    PROGRESS

--- src/components/MainComponent/maincomponent.js
+++ src/components/MainComponent/maincomponent.js
@@ -1,5 +1,6 @@
 import "./maincomponent.css";
 import React from "react";
+import CenteredOverlay from "./centeredOverlay/centeredOverlay";
 import HospitalHours from "./HospitalHours/hospitalhours"; // Ensure correct path
 import { combinedPhotos } from "./arrayOfPhotos"; // Ensure correct path
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -11,33 +12,38 @@ import "swiper/css/autoplay";
 
 const MainComponent = () => {
   return (
-    <div className="mainContainer">
-      <HospitalHours />
-      <div className="contentSide">
-        <div className="arrivalInstructions">
-          <h2>To Minimize Stress for You & Your Pet</h2>
-          <h2>Please Call 484-461-7520 upon arrival</h2>
-          <Swiper
-            spaceBetween={0}
-            slidesPerView={1}
-            loop={true}
-            autoplay={{
-              delay: 4500,
-              disableOnInteraction: false,
-            }}
-            speed={500} // Adds a 500ms transition between slides
-            modules={[Autoplay]} // Add other modules like Navigation, Pagination if you use them
-            className="swiperContainer"
-          >
-            {combinedPhotos.map((currentImage, index) => (
-              <SwiperSlide key={index} className="slide">
-                <img src={currentImage} alt={`Slide ${index}`} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+    <>
+      <div className="mainContainer">
+        <HospitalHours />
+
+        <div className="contentSide">
+          <div className="arrivalInstructions">
+            {/* <h2>To Minimize Stress for You & Your Pet</h2>
+          <h2>Please Call 484-461-7520 upon arrival</h2> */}
+
+            <Swiper
+              spaceBetween={0}
+              slidesPerView={1}
+              loop={true}
+              autoplay={{
+                delay: 4500,
+                disableOnInteraction: false,
+              }}
+              speed={500} // Adds a 500ms transition between slides
+              modules={[Autoplay]} // Add other modules like Navigation, Pagination if you use them
+              className="swiperContainer"
+            >
+              {combinedPhotos.map((currentImage, index) => (
+                <SwiperSlide key={index} className="slide">
+                  <img src={currentImage} alt={`Slide ${index}`} />
+                </SwiperSlide>
+              ))}
+              <CenteredOverlay />
+            </Swiper>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
