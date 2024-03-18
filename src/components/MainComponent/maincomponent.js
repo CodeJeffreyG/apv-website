c89bfa18d95d6db9dcd2cbf99bdf4b963d824c78
Jeffrey <jeffreygraham600@gmail.com>
2024-03-18 12:51:45 -0400

    front image looks better

--- src/components/MainComponent/maincomponent.js
+++ src/components/MainComponent/maincomponent.js
@@ -17,26 +17,25 @@ const MainComponent = () => {
         <div className="arrivalInstructions">
           <h2>To Minimize Stress for You & Your Pet</h2>
           <h2>Please Call 484-461-7520 upon arrival</h2>
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            loop={true}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            speed={500} // Adds a 500ms transition between slides
+            modules={[Autoplay]} // Add other modules like Navigation, Pagination if you use them
+            className="swiperContainer"
+          >
+            {combinedPhotos.map((currentImage, index) => (
+              <SwiperSlide key={index} className="slide">
+                <img src={currentImage} alt={`Slide ${index}`} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-
-        {/* <Swiper
-          spaceBetween={10}
-          slidesPerView={1}
-          loop={true}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          speed={500} // Adds a 500ms transition between slides
-          modules={[Autoplay]} // Add other modules like Navigation, Pagination if you use them
-          className="swiperContainer"
-        >
-          {combinedPhotos.map((currentImage, index) => (
-            <SwiperSlide key={index} className="slide">
-              <img src={currentImage} alt={`Slide ${index}`} />
-            </SwiperSlide>
-          ))}
-        </Swiper> */}
       </div>
     </div>
   );
