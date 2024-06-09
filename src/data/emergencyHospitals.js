47fb9e049ee034040b1a0bc01bdba235d68ca5c3
Jeffrey <jeffreygraham600@gmail.com>
2024-06-09 17:09:43 -0400

    data changes

---
+++ src/data/emergencyHospitals.js
@@ -0,0 +1,33 @@
+const EMERGENCY_HOSPITALS = [
+  {
+    name: "Keystone Veterinary Emergency",
+    location: "Havertown, PA, 19083",
+    hours: "24hr",
+    url: "https://www.thrivepetcare.com/locations/pennsylvania/havertown/keystone-veterinary-emergency-referral?utm_source=google&utm_medium=gmbs",
+    directions: "https://goo.gl/maps/someKeystoneMapLink"
+  },
+  {
+    name: "Blue Pearl Pet Hospital",
+    location: "Philadelphia, PA, 19147",
+    hours: "24hr",
+    url: "https://bluepearlvet.com/hospital/malvern-pa/our-vets/",
+    directions: "https://goo.gl/maps/someBluePearlMapLink"
+  },
+  {
+    name: "Metropolitan Veterinary Associates",
+    location: "Norristown, PA, 19403",
+    hours: "24hr",
+    url: "https://metro-vet.com/?gclid=CjwKCAjw17qvBhBrEiwA1rU9w7PXCCZz5uWOc7LWCtICs0EV-1LdC7KbTeuMuBuvCgniC1AS3jsF3hoCsRIQAvD_BwE",
+    directions: "https://goo.gl/maps/someMetropolitanMapLink"
+  },
+  {
+    name: "VRC - Veterinary Referral Center",
+    location: "Malvern, PA, 19355",
+    hours: "24hr",
+    url: "https://www.vrcmalvern.com/",
+    directions: "https://goo.gl/maps/someVRCMapLink"
+  }
+];
+
+
+  export default EMERGENCY_HOSPITALS;
---
+++ src/data/exoticPatientHospitals.js
@@ -0,0 +1,32 @@
+// Assuming EXOTIC_PATIENT_HOSPITALS is defined similarly
+const EXOTIC_PATIENT_HOSPITALS = [
+  {
+    name: "VEG - Veterinary Emergency Group",
+    location: "Philadelphia, PA, 19107",
+    hours: "24hr",
+    url: "https://veterinaryemergencygroup.com/",
+    directions: "https://goo.gl/maps/someVEGMapLink"
+  },
+  {
+    name: "Mount Laurel Animal Hospital",
+    location: "Mount Laurel Township, NJ, 08054",
+    hours: "24hr",
+    url: "https://mlahvet.com/",
+    directions: "https://goo.gl/maps/someMountLaurelMapLink"
+  },
+  {
+    name: "Radnor Veterinary Hospital",
+    location: "Wayne, PA, 19087",
+    url: "https://radnorvet.com/",
+    directions: "https://goo.gl/maps/someRadnorMapLink"
+  },
+  {
+    name: "Avian & Exotic Medical Center",
+    location: "Aston, PA, 19130",
+    url: "https://avianexoticvetpa.com/",
+    directions: "https://goo.gl/maps/someAvianExoticMapLink"
+  }
+];
+
+
+  export default EXOTIC_PATIENT_HOSPITALS;
